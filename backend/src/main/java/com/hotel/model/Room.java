2a47c9df101631fa3d585ca2763fd7599936a509
arianecasas <ariane.casas@nectarcrm.com.br>
2026-02-23 11:14:12 -0300

    feat: added Room entity

---
+++ backend/src/main/java/com/hotel/model/Room.java
@@ -0,0 +1,29 @@
+package com.hotel.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "rooms")
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    @Column(nullable = false)
+    private Double pricePerNight;
+    private String imageUrl;
+    @ElementCollection
+    @CollectionTable(name = "room_features", joinColumns = @JoinColumn(name = "room_id"))
+    @Column(name = "feature")
+    private List<String> features;
+}
