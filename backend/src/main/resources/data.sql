75e453c55f4d5b22d7ccfbce93724e2023eb1112
arianecasas <ariane.casas@nectarcrm.com.br>
2026-02-23 14:27:34 -0300

    chore: config for data to be executed after hibernate

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO rooms (name, description, price_per_night, image_url) VALUES
+                                                                      ('Deluxe Suite', 'A spacious suite with ocean view', 350.00, 'https://images.unsplash.com/photo-1631049307264-da0ec9d70304'),
+                                                                      ('Standard Room', 'Comfortable room with all amenities', 120.00, 'https://images.unsplash.com/photo-1566073771259-6a8506099945'),
+                                                                      ('Penthouse', 'Luxury penthouse with panoramic views', 800.00, 'https://images.unsplash.com/photo-1582719478250-c89cae4dc85b');
